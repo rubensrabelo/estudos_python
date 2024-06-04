6b75ec7070ea2a93356df4a7ffd1bec6a88cd4c8
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-04 09:56:06 -0300

    Test

--- estatistica/descritiva/src/estatistica_descritiva/tab_frequencia.py
+++ estatistica/descritiva/src/estatistica_descritiva/tab_frequencia.py
@@ -1,24 +1,28 @@
 import itertools
-from dataclasses import dataclass, field
-from typing import List
-
-# import pandas as pd
-# from random import randint
 
 
-@dataclass
-class TabelaFrequencia:
-        numeros: List[int] = field(default_factory=List)
+class teste:
+    def __init__(self, lista):
+        self.lista = sorted(lista)
+        self.dicionario = {}
+
+    def addDicionario(self):
+        gr = itertools.groupby(self.lista)
+        for c, v in gr:
+            self.dicionario[c] = len(list(v))
+
+    def __str__(self):
+        return "\n".join(f"{c}: {v}" for c, v in self.dicionario)
 
 
 if __name__ == "__main__":
-    lista_num = [1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 9, 9]
-
-    tab = TabelaFrequencia(lista_num)
-
-    # Levar o código abaixo para a classe:
-    # values = itertools.groupby(lista_num)
-    # d = {}
-    # for key, value in values:
-    #       d[key] = len(list(value))
-    # print(d)
+    lista = [1, 1, 2, 3, 4, 4]
+    dicionario = {}
+    t = teste(lista)
+    print(t)
+    gr = itertools.groupby(lista)
+    
+    for c, v in gr:
+        dicionario[c] = len(list(v))
+    
+    print(dicionario)

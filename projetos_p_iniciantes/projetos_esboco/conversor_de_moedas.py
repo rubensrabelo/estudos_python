455be62d457e0b385312e4242d0bcd7d5346b3e6
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-17 16:11:29 -0300

    Currency Converter

---
+++ projetos_p_iniciantes/projetos_esboco/conversor_de_moedas.py
@@ -0,0 +1,6 @@
+valor_atual_moeda_conversao = float(input("Digite o valor atual da moeda de conversao: "))
+valor_de_conversao = float(input("Qual a quantidade de dinheiro para conversao? "))
+
+conversao = valor_atual_moeda_conversao * valor_de_conversao
+
+print(f"O valor de conversao e {conversao}")

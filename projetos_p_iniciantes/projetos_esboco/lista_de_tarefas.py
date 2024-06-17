8d12d6b16c7cef80704a7435dfaef04d2d52e875
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-17 16:16:00 -0300

    Removing calculator and Creating to do list

---
+++ projetos_p_iniciantes/projetos_esboco/lista_de_tarefas.py
@@ -0,0 +1,25 @@
+# Lista de Tarefas
+
+lista_de_tarefas = []
+
+
+def inserir_tarefa():
+    ...
+
+
+def remover_tarefa():
+    ...
+
+
+def visualizar_tarefas():
+    ...
+
+
+funcionalidades_de_opcoes = {
+    1: "INSERIR TAREFA",
+    2: "REMOVER TAREFA",
+    3: "VISUALIZAR TAREFAS",
+}
+
+while True:
+    ...

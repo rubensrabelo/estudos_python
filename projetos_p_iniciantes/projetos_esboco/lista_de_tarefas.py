0bec0d39214516d6c651478396dd84aafffeba0a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-17 16:26:50 -0300

    Editing to do list

--- projetos_p_iniciantes/projetos_esboco/lista_de_tarefas.py
+++ projetos_p_iniciantes/projetos_esboco/lista_de_tarefas.py
@@ -11,15 +11,40 @@ def remover_tarefa():
     ...
 
 
+def marcar_conclusao():
+    ...
+
+
 def visualizar_tarefas():
     ...
 
 
-funcionalidades_de_opcoes = {
-    1: "INSERIR TAREFA",
-    2: "REMOVER TAREFA",
-    3: "VISUALIZAR TAREFAS",
+opcoes_do_programa = {
+    "1": "Inserir Tarefa",
+    "2": "Remover Tarefa",
+    "3": "Visualizar Tarefas",
+    "4": "Marcar como Concluído",
+    "5": "Sair do Programa",
 }
 
 while True:
-    ...
+    print("Escolha uma opção: ")
+    for valor, chave in opcoes_do_programa.items():
+        print(f"[{valor}]: {chave}")
+    print()
+    opcao = str(input("Digite sua o5pção: "))
+
+    if opcao in opcoes_do_programa:
+        if opcao == "1":
+            inserir_tarefa()
+        elif opcao == "2":
+            remover_tarefa()
+        elif opcao == "3":
+            visualizar_tarefas()
+        elif opcao == "4":
+            marcar_conclusao()
+        else:
+            break
+    else:
+        print("Digite uma opção válida!")
+    print()

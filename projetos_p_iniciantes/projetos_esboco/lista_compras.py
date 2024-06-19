02b13da968df3a267ee5f408bfbbe33b142547ff
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-19 14:05:35 -0300

    Store list program

--- projetos_p_iniciantes/projetos_esboco/lista_compras.py
+++ projetos_p_iniciantes/projetos_esboco/lista_compras.py
@@ -10,16 +10,21 @@ opcoes = {
 }
 
 
-def adicionar():
-    ...
+def adicionar(nome, preco):
+    lista_de_compras.append({
+        "nome": nome,
+        "preco": preco,
+    })
 
 
-def remover():
-    ...
+def remover(indice):
+    del lista_de_compras[indice]
 
 
 def visualizar():
-    ...
+    return "\n".join(
+        f"[{i}] Nome: {produto['nome']} e Preco: {produto['preco']}" for i, produto in enumerate(lista_de_compras)
+    )
 
 
 while True:
@@ -31,14 +36,16 @@ while True:
     opcao = input("Digite uma opcao: ")
 
     if opcao in opcoes:
-        if opcao == 1:
-            adicionar()
-        if opcao == 1:
-            remover()
-        if opcao == 1:
-            visualizar()
+        if opcao == "1":
+            nome = input("Digite o nome do produto: ")
+            preco = float(input("Digite o valor do produto: "))
+            adicionar(nome, preco)
+        elif opcao == "2":
+            indice = int(input("Digite o indice do produto: "))
+            remover(indice)
+        elif opcao == "3":
+            print(visualizar())
         else:
             break
     else:
         print("Digite uma opcao valida!")
-    ...

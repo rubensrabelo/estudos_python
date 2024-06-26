bd264b19a9d87b379c77ca77783c742c9e6a6644
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-26 15:55:12 -0300

    Store

---
+++ store/resources/user.py
@@ -0,0 +1,55 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
+
+from db import db
+from models import UserModel
+from schemas import UserSchema
+
+
+blp = Blueprint("Users", "users", descriptions="Operation on user")
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            abort(404, message="A user with that already exists")
+        user = UserModel(
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"])
+        )
+        db.session.add(user)
+        db.session.commit()
+        return {"message": "User created successfully."}, 201
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user =  UserModel.query.filter(UserModel.username == user_data["username"]).first()
+        if (
+            user and
+            pbkdf2_sha256.verify(user_data["password"], user.password)
+        ):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+        abort(401, message="Invalid credentials")
+
+
+@blp.route("/register/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200

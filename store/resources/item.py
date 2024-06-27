bac06175fb0a0eb8a0f783aaf247d1606fd0b81b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-27 08:18:27 -0300

    Store

--- store/resources/item.py
+++ store/resources/item.py
@@ -1,7 +1,6 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extented import jwt_required
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
@@ -14,11 +13,13 @@ blp = Blueprint("items", __name__, descriptions="Operation on item")
 
 @blp.route("/item/<string:item_id")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
+    @jwt_required()
     def delete(item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -42,10 +43,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList:
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(200, ItemSchema)
     def post(self, item_data):

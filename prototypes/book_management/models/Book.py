2488924bbe367be1bc774624c637d058c84323f5
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-01 09:13:13 -0300

    Book Managements --> Improving the book class and Creating the database

--- prototypes/book_management/models/Book.py
+++ prototypes/book_management/models/Book.py
@@ -3,24 +3,56 @@ from models.PublishingCompany import PublishingCompany
 
 
 class Book:
-    id: int = 0
+    """Classe que representa um livro."""
 
-    def __init__(self, name: str, pages_quantity: int, price: float, publishingCompany: PublishingCompany) -> None:
-        self.name = name
-        self.pages_quantity = pages_quantity
-        self.price = price
-        self.publishingCompany = publishingCompany
+    __id_counter: int = 0  # Variável de classe privada para rastrear IDs únicos
+
+    def __init__(self, name: str, pages_quantity: int, price: float, publishing_company: PublishingCompany) -> None:
+        """
+        Inicializa uma instância de Book.
+
+        Args:
+            name (str): O nome do livro.
+            pages_quantity (int): A quantidade de páginas do livro.
+            price (float): O preço do livro.
+            publishing_company (PublishingCompany): A empresa editora do livro.
+        """
+        self.name: str = name
+        self.pages_quantity: int = pages_quantity
+        self.price: float = price
+        self.publishingCompany: PublishingCompany = publishing_company
         self.authors: list[Author] = []
-        Book.id += 1
+        self.id = self.__increment_id()
+
+    def __increment_id(self) -> int:
+        """Incrementa o ID do livro e retorna o novo ID."""
+        Book.__id_counter += 1
+        return Book.__id_counter
+
+    @staticmethod
+    def reset_id_counter() -> None:
+        """Reseta o contador de IDs."""
+        Book.__id_counter = 0
 
     def add_author(self, author: Author):
+        """Adiciona um autor à lista de autores do livro.
+
+        Args:
+            author (Author): O autor a ser adicionado.
+        """
         self.authors.append(author)
 
     def remove_author(self, author: Author):
+        """Remove um autor da lista de autores do livro.
+
+        Args:
+            author (Author): O autor a ser removido.
+        """
         self.authors.remove(author)
 
     def __str__(self) -> str:
+        """Retorna uma representação em string do livro."""
         first_part = f"{__class__.__name__}(Name={self.name}, Pages Quantity={self.pages_quantity}"
-        second_part = f", Price={self.price}, Publishing Company={self.publishingCompany.name}"
+        second_part = f", Price={self.price}, Publishing Company={self.publishing_company.name}"
         third_part = ", Authors=[" + ", ".join(str(author.name) for author in self.authors) + "])"
         return first_part + second_part + third_part

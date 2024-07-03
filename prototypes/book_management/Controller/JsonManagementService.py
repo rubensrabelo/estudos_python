aa8844b43a32548ee6eb6131ddba63027cf84d68
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-03 08:18:50 -0300

    Book Management

--- prototypes/book_management/Controller/JsonManagementService.py
+++ prototypes/book_management/Controller/JsonManagementService.py
@@ -1,12 +1,33 @@
+import os
+import json
+from typing import Union
+
+
 class JsonManagementService:
-    def open():
-        ...
+    __PATH_DIR = r"C:\Users\ruben\OneDrive\01. estudo\01_linguagem_programacao\02_python\estudos_python\prototypes\book_management\database"
 
-    def add():
-        ...
+    def __init__(self, file: str) -> None:
+        self.__file: str = file
+        self.__PATH_FILE: str = os.path.join(JsonManagementService.__PATH_DIR, file)
 
-    def update():
-        ...
+    def open_file(self) -> list[dict[str, Union[str, float]]]:
+        with open(self.__PATH_FILE, "r", encoding="utf-8") as file_read:
+            data = json.load(file_read)
+        return data
 
-    def show():
-        ...
+    def add_file(self, value):
+        data = self.open_file()
+        data.append(value)
+
+        with open(self.__PATH_FILE, "w") as file_write:
+            json.dump(data, file_write, indent=4)
+
+    def update_file(self, index):
+        data = self.open_file()
+
+        with open(self.__PATH_FILE) as file:
+            ...
+    # Repensar se vale a pena construir essa classe
+    # def show_file(self):
+    #     with open(self.__PATH_FILE) as file:
+    #         ...

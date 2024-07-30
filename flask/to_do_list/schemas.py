2ac035e5c218d4e99451f9e8c71837d54f3b86ec
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-30 16:38:05 -0300

    Creating the shema and model tasks

--- flask/to_do_list/schemas.py
+++ flask/to_do_list/schemas.py
@@ -1,8 +1,14 @@
-from mashmallow import Schema, fields
+from marshmallow import Schema, fields
 
 
 class TaskSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     status = fields.Int()
-    expiration_date = fields.Date() # Ver se está correto essa parte
+    create_at = fields.DateTime(dump_only=True)
+    update_at = fields.DateTime(dump_only=True)
+
+
+class TaskUpdateSchema(Schema):
+    name = fields.Str(required=True)
+    status = fields.Int()

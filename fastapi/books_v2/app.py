e7f92f04b58cc089aad916d1e96ac0b05cff02f7
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-04 16:27:46 -0300

    Ending book version two with fastapi

--- fastapi/books_v2/app.py
+++ fastapi/books_v2/app.py
@@ -78,10 +78,16 @@ async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
 
 @app.get("/books/publish/", status_code=status.HTTP_200_OK)
 async def read_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
-    ...
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+
+    return books_to_return
 
 
-@app.post("/create-book", status_code=status.HTTP_201_CREATED)
+@app.post("/books/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
@@ -90,3 +96,30 @@ async def create_book(book_request: BookRequest):
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.delete("/books/delete-book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")

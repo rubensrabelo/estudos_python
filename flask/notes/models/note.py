54e04217e576932830997ac2969df44855cb7a8b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-16 14:22:37 -0300

    Conflict

--- flask/notes/models/note.py
+++ flask/notes/models/note.py
@@ -3,26 +3,6 @@ from datetime import datetime, timezone
 from db import db
 
 
-<<<<<<< HEAD
-class Note(db.Model):
-    __tablename__ = "tb_notes"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=True)
-    description = db.Column(db.String, nullable=True)
-    create_at = db.Column(
-        db.DateTime, nullable=False,
-        default=lambda: datetime.now(tz=timezone.utc)
-    )
-    update_at = db.Column(
-        db.DateTime, nullable=False,
-        default=lambda: datetime.now(tz=timezone.utc),
-        onupdate=lambda: datetime.now(tz=timezone.utc)
-    )
-    
-    # Quero entender como funciona essa realação aqui
-    tags = db.relationship("TagModel", back_populates="")
-=======
 class NoteModel(db.Model):
     __tablename__ = "tb_notes"
 
@@ -44,4 +24,3 @@ class NoteModel(db.Model):
     tags = db.relationship(
         "TagModel", back_populates="notes", secondary="tb_notes_tags"
         )
->>>>>>> working
--- flask/notes/models/tag.py
+++ flask/notes/models/tag.py
@@ -1,13 +1,6 @@
 from db import db
 
 
-<<<<<<< HEAD
-class TagModel(db.Model):
-    __tablename__ = "tb_tags"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-=======
 class TagsModel(db.Model):
     __tablename__ = "tb_tags"
 
@@ -17,4 +10,3 @@ class TagsModel(db.Model):
     notes = db.relationship(
         "NoteModel", back_populates="tags", secondary="tb_notes_tags"
         )
->>>>>>> working

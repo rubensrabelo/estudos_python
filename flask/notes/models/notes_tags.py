eb1c96204bb053929bea10b85c7d88c5f11dd67f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-20 09:50:48 -0300

    Removing all folders

--- flask/notes/models/notes_tags.py
+++
@@ -1,9 +0,0 @@
-from db import db
-
-
-class NotesTagsModel(db.Model):
-    __tablename__ = "tb_notes_tags"
-
-    id = db.Column(db.Integer, primary_key=True)
-    note_id = db.Column(db.Integer, db.ForeignKey("tb_notes.id"))
-    tag_id = db.Column(db.Integer, db.ForeignKey("tb_tags.id"))

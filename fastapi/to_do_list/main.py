43fa71f64b3ed7785fcbb3f279113a43c77b7a99
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-22 15:06:19 -0300

    Reading all data of the database with fastapi

--- fastapi/to_do_list/main.py
+++ fastapi/to_do_list/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def get_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()

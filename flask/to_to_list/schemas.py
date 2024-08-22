439cffcd3df929982dc5ca76be75aee06e3ad99f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-22 15:40:54 -0300

    Implementing and testing the api

--- flask/to_to_list/schemas.py
+++ flask/to_to_list/schemas.py
@@ -4,11 +4,11 @@ from marshmallow import Schema, fields
 class TaskSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
-    name = fields.Bool()
-    create_at = fields.DateTime(dump_only=True)
-    update_at = fields.DateTime(dump_only=True)
+    status = fields.Bool()
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
 
 
 class TaskUpdateSchema(Schema):
     name = fields.Str(required=True)
-    name = fields.Bool()
+    status = fields.Bool()

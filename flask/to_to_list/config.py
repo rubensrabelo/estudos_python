6289274b31e3aa99e1c4424ad691fb8a56ef68b5
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-25 09:49:00 -0300

    Inserting config.py

---
+++ flask/to_to_list/config.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Config:
+    API_TITLE = "To Do List REST API"
+    API_VERSION = "v2"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
+
+    @staticmethod
+    def get_db_url():
+        return (
+            f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}"
+            f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        )

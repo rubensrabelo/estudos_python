6c148e904871552aa5d73a20bbc7320b22c384b7
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-26 08:27:08 -0300

    Implementing authetication

--- fastapi/to_do_list/routers/auth.py
+++ fastapi/to_do_list/routers/auth.py
@@ -1,3 +1,32 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from passlib.context import CryptContext
+
+from models import Users
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def create_user(user_request: CreateUserRequest):
+    user_model = Users(
+        username=user_request.username,
+        email=user_request.email,
+        first_name=user_request.first_name,
+        last_name=user_request.last_name,
+        role=user_request.role,
+        hashed_password=bcrypt_context.hash(user_request.password),
+        is_active=True
+    )
+
+    return user_model

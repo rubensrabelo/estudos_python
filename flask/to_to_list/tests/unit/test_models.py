9b2e54d378ef92f4d34d0125b51051304e2736d3
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-29 16:13:13 -0300

    Implementig create and update tests with flask

--- flask/to_to_list/tests/unit/test_models.py
+++ flask/to_to_list/tests/unit/test_models.py
@@ -1,16 +1,43 @@
+from datetime import datetime, timedelta
+
 from models.task import TaskModel
 from db import db
 
 
+# 1. Task creation test
 def test_task_model(client):
-    new_task = TaskModel(title="Test Task", description="This is a test task")
+    new_task = TaskModel(name="Test Task")
 
     db.session.add(new_task)
     db.session.commit()
 
-    task = TaskModel.query.filter_by(title="Test Task").first()
+    task = TaskModel.query.filter_by(name="Test Task").first()
 
     assert task is not None
-    assert task.title == "Test Task"
-    assert task.status == False
-    assert task.description == "This is a test task"
+    assert task.name == "Test Task"
+    assert task.status is False
+
+    now = datetime.now()
+    assert task.created_at <= now
+    assert task.created_at >= now - timedelta(seconds=1)
+    assert task.updated_at <= now
+    assert task.updated_at >= now - timedelta(seconds=1)
+
+
+# 2. Task update task
+def test_task_update(client):
+    new_task = TaskModel(name="Test Task")
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    task = TaskModel.query.filter_by(name="Test Task").first()
+
+    task.name = "Update Task"
+    db.session.commit()
+
+    update_task = TaskModel.query.filter_by(name="Update Task").first()
+
+    assert update_task is not None
+    assert update_task.name == "Update Task"
+    assert update_task.updated_at > task.created_at

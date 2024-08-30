6089e925958ec9dcc42ad5ffc8f3e0066d104ac2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-30 15:40:11 -0300

    Ending authorization

--- fastapi/to_do_list/routers/auth.py
+++ fastapi/to_do_list/routers/auth.py
@@ -56,8 +56,8 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
-def create_acess_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": username, "id": user_id}
+def create_acess_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id, "role": role}
     expires = datetime.now(timezone.utc) + expires_delta
 
     encode.update({"exp": expires})
@@ -70,11 +70,12 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: str = payload.get("id")
+        user_role: str = payload.get("role")
 
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
 
-        return {"username": username, "id": user_id}
+        return {"username": username, "id": user_id, "user_role": user_role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
 
@@ -105,6 +106,6 @@ async def login_for_acess_token(
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
 
-    token = create_acess_token(user.username, user.id, timedelta(minutes=20))
+    token = create_acess_token(user.username, user.id, user.role, timedelta(minutes=20))
 
     return {"access_token": token, "token_type": "bearer"}
